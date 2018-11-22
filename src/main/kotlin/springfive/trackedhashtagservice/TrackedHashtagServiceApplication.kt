55303974c2a800d3a81be6d0dc6314d867d746e7
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2018-11-22 14:07:39 +0100

    First commit

---
+++ src/main/kotlin/springfive/trackedhashtagservice/TrackedHashtagServiceApplication.kt
@@ -0,0 +1,11 @@
+package springfive.trackedhashtagservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TrackedHashtagServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<TrackedHashtagServiceApplication>(*args)
+}
